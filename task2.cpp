3cfbb2129d1147ee21726235c45de7b20e4ace03
Artem <inokentijinokentiev6@gmail.com>
2024-10-31 12:47:13 +0100

    string

--- task2.cpp
+++ task2.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-	char* name;
+	string name;
 	cout << "Enter your name" << endl;
 	cin >> name;
-	cout << "Hello, " << name << endl;
+	cout << "Hello " << name << endl;
 }
 
 
